c001322b503268518545b9d08fcf4069bb25eaab
Matti Aho <ookooko@gmail.com>
2018-07-11 16:09:09 +0300

    baseurlia muutettuko

--- src/index.js
+++ src/index.js
@@ -1,33 +1,9 @@
 // osa2 single page application
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// console.log(promise2);
-
-/*
-const notes = [
-  {
-    id: 1,
-    content: 'HTML on helppoa',
-    date: '2017-12-10T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Selain pystyy suorittamaan vain javascriptiä',
-    date: '2017-12-10T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-    date: '2017-12-10T19:20:14.298Z',
-    important: true
-  }
-];*/
 
 
 
